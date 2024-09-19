171957e5c68f931c5c25bc40baef85b7c5e19aae
Mathesh M <157198679+Mathesh-299@users.noreply.github.com>
2024-09-19 15:56:37 +0530

    fully

---
+++ reduxsample/src/redux/useSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userData = createSlice({
+    name: "userData",
+    initialState: {
+        userData:null
+    },
+    reducers: {
+        login:(state,action)
+    }
+
+})
+
+export const { invalidate } = sliceName.actions
+export default sliceName.reducer
--- sample1/tailwind.config.js
+++ sample1/tailwind.config.js
@@ -9,3 +9,33 @@ export default {
   },
   plugins: [],
 }
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     './pages/**/*.{html,js}',
+//     './components/**/*.{html,js}',
+//   ],
+//   // ...
+// }
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   theme: {
+//     screens: {
+//       'sm': '640px',
+//       // => @media (min-width: 640px) { ... }
+
+//       'md': '768px',
+//       // => @media (min-width: 768px) { ... }
+
+//       'lg': '1024px',
+//       // => @media (min-width: 1024px) { ... }
+
+//       'xl': '1280px',
+//       // => @media (min-width: 1280px) { ... }
+
+//       '2xl': '1536px',
+//       // => @media (min-width: 1536px) { ... }
+//     }
+//   }
+// }
