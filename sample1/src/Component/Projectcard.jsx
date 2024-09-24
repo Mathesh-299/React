74ee7cc2fcb5bc00b3c36908d5e1835d6c021bf3
Mathesh M <157198679+Mathesh-299@users.noreply.github.com>
2024-09-24 13:08:28 +0530

    commit

---
+++ sample1/src/Component/Projectcard.jsx
@@ -0,0 +1,8 @@
+import axios from 'axios'
+const APl="https://66ee897f3ed5bb4d0bf131b4.mockapi.io"
+const getProjects = () => axios.get(`${API}/projects`)
+const getProjectbyID = (id) => axios.get(`${API}/projects/${id}`)
+const addProject = (projectdata) => axios.post(`${API}/projects`, projectdata)
+const editProject = (id, projectdata) => axios.put(`${API}/projects/${id}`, projectdata)
+const deleteProject = (id) => axios.delete(`${API}/projects/${id}`)
+export { addProject, deleteProject, editProject, getProjectbyID, getProjects }
